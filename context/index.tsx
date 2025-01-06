a41c93fdc4b5cf69e213e8dd223deb4a6cfbbb7f
Xirynx <64374129+Xirynx@users.noreply.github.com>
2025-01-06 20:36:02 +0200

    feat: Initialise telegram mini apps sdk

--- context/index.tsx
+++ context/index.tsx
@@ -2,12 +2,13 @@
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React, { type ReactNode } from 'react';
+import { init } from '@telegram-apps/sdk-react';
 
 // Set up queryClient
 const queryClient = new QueryClient();
 
 function ContextProvider({ children }: { children: ReactNode; }) {
-    
+    init();
     return (
         <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     );
