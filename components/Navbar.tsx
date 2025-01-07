2c7f3fbdb0436b8b7b577a03070797b4e3791a93
io <stuart@kieser.co.za>
2025-01-07 09:24:56 +0200

    Navbar fixes and animiation delay

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,14 +9,21 @@ import {
   BrainCircuit,
   Wallet,
   Info,
-  Scale,
 } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 export const Navbar: React.FC<{ className?: string }> = ({ className }) => {
   const [activeIndex, setActiveIndex] = React.useState<number | null>(0);
+  const router = useRouter();
 
-  const handleButtonClick = (index: number) => {
+  const handleButtonClick = (index: number, route: string) => {
     setActiveIndex(index);
+
+    setTimeout(() => {
+      if (typeof window !== "undefined") {
+        router.push(route);
+      }
+    }, 400);
   };
 
   return (
@@ -26,27 +33,30 @@ export const Navbar: React.FC<{ className?: string }> = ({ className }) => {
         className
       )}
     >
-      {[Wallet, ArrowDownUp, BrainCircuit, ChartCandlestick, Info].map(
-        (Icon, index) => (
-          <Button
-            key={index}
-            onClick={() => handleButtonClick(index)}
-            className={cn(
-              "h-12 w-12 rounded-full text-white shadow-none font-bold bg-accent",
-              activeIndex === index ? "active:bg-accent" : "bg-accent"
-            )}
-            style={{
-              transform:
-                activeIndex === index ? "translateY(-22px)" : "translateY(0)", // Slight upward shift
-              backgroundColor:
-                activeIndex === index ? "#272729" : "transparent",
-              transition: "transform 0.5s ease", // Smooth transition
-            }}
-          >
-            <Icon />
-          </Button>
-        )
-      )}
+      {[
+        { icon: Wallet, route: "/" },
+        { icon: ArrowDownUp, route: "/swap" },
+        { icon: BrainCircuit, route: "/circuit" },
+        { icon: ChartCandlestick, route: "/candlestick" },
+        { icon: Info, route: "/info" },
+      ].map((obj, index) => (
+        <Button
+          key={index}
+          onClick={() => handleButtonClick(index, obj.route)}
+          className={cn(
+            "h-12 w-12 rounded-full text-white shadow-none font-bold bg-accent",
+            activeIndex === index ? "active:bg-accent" : "bg-accent"
+          )}
+          style={{
+            transform:
+              activeIndex === index ? "translateY(-22px)" : "translateY(0)", // Slight upward shift
+            backgroundColor: activeIndex === index ? "#272729" : "transparent",
+            transition: "transform 0.4s ease", // Smooth transition
+          }}
+        >
+          <obj.icon />
+        </Button>
+      ))}
     </div>
   );
 };
--- context/index.tsx
+++ context/index.tsx
@@ -1,17 +1,17 @@
-'use client';
+"use client";
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import React, { type ReactNode } from 'react';
-import { init } from '@telegram-apps/sdk-react';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React, { type ReactNode } from "react";
+import { init } from "@telegram-apps/sdk-react";
 
 // Set up queryClient
 const queryClient = new QueryClient();
 
-function ContextProvider({ children }: { children: ReactNode; }) {
-    init();
-    return (
-        <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-    );
+function ContextProvider({ children }: { children: ReactNode }) {
+  // init();
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
 }
 
 export default ContextProvider;
