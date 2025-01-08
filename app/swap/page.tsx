64dbc665282aaa64c60027cc3f9d68cab1275430
io <stuart@kieser.co.za>
2025-01-08 09:38:10 +0200

    Deleted Old Modal Replaced with ShadCn, imporved navbar

--- app/swap/page.tsx
+++ app/swap/page.tsx
@@ -1,114 +1,117 @@
 "use client";
 
 import { Button } from "@/components/shadcn/button";
-import TokenListModal from "@/components/TokenListModal";
 import { ArrowDownUpIcon, ChevronDown } from "lucide-react";
 import { useState } from "react";
+import DrawerModal from "@/components/DrawerModal";
 
-export default function SwapPage() {
-  const [typeAction, setAction] = useState("");
+interface SellCompProps {
+  handleToggleModal: (sell: string) => void;
+}
 
-  const SellComp = () => {
-    return (
-      <div
-        className="bg-gray-700 shadow-md rounded-[10px] p-2 flex flex-col gap-2"
-        onClick={() => handleToggleModal("sell")}
-      >
-        <div className="">
-          <span className="opacity-70 text-sm">Sell</span>
-        </div>
-        <div className="flex justify-around items-center text-xl text-left">
-          <input
-            className="w-2/3 bg-transparent text-white placeholder-white placeholder-opacity-70"
-            placeholder="0.0"
-          />
-
-          <Button className="rounded-full bg-gray-600 flex text-white gap-4 w-32">
-            <img src="/ethereum.webp" className="w-6 h-6 rounded-full" />
-
-            <p>ETH</p>
-            <ChevronDown />
-          </Button>
-        </div>
-
-        <div className="text-sm">
-          <span className="flex opacity-70 items-baseline">
-            <p className="text-lg">$ 0</p>
-            <p className="text-sm">.00</p>
-          </span>
-        </div>
+const SellComp = ({ handleToggleModal }: SellCompProps) => {
+  return (
+    <div className="bg-gray-500 shadow-md rounded-[10px] p-2 flex flex-col gap-2">
+      <div className="">
+        <span className="opacity-80 text-sm">Sell</span>
       </div>
-    );
-  };
-
-  const BuyComp = () => {
-    return (
-      <div className="bg-purple-500 shadow-md rounded-[10px] p-2 flex flex-col gap-2">
-        <div className="">
-          <span className="opacity-80 text-sm">Buy</span>
-        </div>
-        <div className="flex justify-around text-xl text-left">
-          <input
-            className="w-2/3 bg-transparent text-white placeholder-white placeholder-opacity-80"
-            placeholder="0.0"
-          />
-
-          <Button
-            className="rounded-full bg-purple-600 flex text-white gap-4 w-32"
-            onClick={() => handleToggleModal("buy")}
-          >
-            <img src="/Sushi.webp" className="w-6 h-6 rounded-full" />
-
-            <p>Sushi</p>
-            <ChevronDown />
-          </Button>
-        </div>
-        <div className="text-sm text-white">
-          <span className="flex opacity-80 items-baseline">
-            <p className="text-lg">$ 0</p>
-            <p className="text-sm">.00</p>
-          </span>
-        </div>
-      </div>
-    );
-  };
-
-  const SwapComp = () => {
-    return (
-      <div className="flex justify-center">
-        <Button className="bg-gray-600 rounded-full size-12 border-2 shadow-md border-gray-500 active:bg-gray-600">
-          <ArrowDownUpIcon color="white" />
+      <div className="flex justify-around text-xl text-left">
+        <input
+          className="w-2/3 bg-transparent text-primary-foreground placeholder-white placeholder-opacity-80"
+          placeholder="0.0"
+        />
+        <Button
+          className="rounded-full bg-accent flex text-primary-foreground gap-4 w-32"
+          onClick={() => handleToggleModal("Sell")}
+        >
+          <img src="/ethereum.webp" className="w-6 h-6 rounded-full" />
+          <p>ETH</p>
+          <ChevronDown />
         </Button>
       </div>
-    );
-  };
+      <div className="text-sm text-primary-foreground">
+        <span className="flex opacity-80 items-baseline">
+          <p className="text-lg">$ 0</p>
+          <p className="text-sm">.00</p>
+        </span>
+      </div>
+    </div>
+  );
+};
 
+interface BuyCompProps {
+  handleToggleModal: (buy: string) => void;
+}
+
+const BuyComp = ({ handleToggleModal }: BuyCompProps) => {
+  return (
+    <div className="bg-primary shadow-md rounded-[10px] p-2 flex flex-col gap-2">
+      <div className="">
+        <span className="opacity-80 text-sm">Buy</span>
+      </div>
+      <div className="flex justify-around text-xl text-left">
+        <input
+          className="w-2/3 bg-transparent text-primary-foreground placeholder-white placeholder-opacity-80"
+          placeholder="0.0"
+        />
+        <Button
+          className="rounded-full bg-accent flex text-primary-foreground gap-4 w-32"
+          onClick={() => handleToggleModal("Buy")}
+        >
+          <img src="/Sushi.webp" className="w-6 h-6 rounded-full" />
+          <p>Sushi</p>
+          <ChevronDown />
+        </Button>
+      </div>
+      <div className="text-sm text-primary-foreground">
+        <span className="flex opacity-80 items-baseline">
+          <p className="text-lg">$ 0</p>
+          <p className="text-sm">.00</p>
+        </span>
+      </div>
+    </div>
+  );
+};
+
+const SwapComp = () => {
+  return (
+    <div className="flex justify-center">
+      <Button className="bg-accent rounded-full size-12">
+        <ArrowDownUpIcon color="white" />
+      </Button>
+    </div>
+  );
+};
+
+const SwapPage = () => {
   const [isOpen, setOpen] = useState(false);
+  const [typeAction, setAction] = useState("");
 
-  const handleToggleModal = (arg: string) => {
+  const handleToggleModal = (action: string) => {
+    setAction(action);
     setOpen(!isOpen);
-    setAction(arg);
   };
 
   return (
     <div className="flex flex-col items-center h-screen bg-transparent p-12">
       <div className="flex flex-col gap-2 w-full">
         {/* Sell Comp */}
-        <SellComp />
+        <SellComp handleToggleModal={handleToggleModal} />
 
         {/* Swap Feature */}
         <SwapComp />
 
         {/* Buy Comp */}
-        <BuyComp />
+        <BuyComp handleToggleModal={handleToggleModal} />
 
-        {isOpen ? (
-          <TokenListModal
-            handleToggleModal={handleToggleModal}
-            typeAction={typeAction}
-          />
-        ) : null}
+        <DrawerModal
+          handleToggleModal={handleToggleModal}
+          typeAction={typeAction}
+          isOpen={isOpen}
+        />
       </div>
     </div>
   );
-}
+};
+
+export default SwapPage;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React from "react";
-import { Button } from "@/components/shadcn/button";
 import { cn } from "@/lib/utils";
 import {
   ChartCandlestick,
@@ -29,33 +28,34 @@ export const Navbar: React.FC<{ className?: string }> = ({ className }) => {
   return (
     <div
       className={cn(
-        "flex justify-evenly w-screen bottom-0 h-16 items-center bg-accent",
+        "flex justify-evenly w-screen bottom-0 h-20 bg-accent",
         className
       )}
     >
       {[
-        { icon: Wallet, route: "/" },
-        { icon: ArrowDownUp, route: "/swap" },
-        { icon: BrainCircuit, route: "/circuit" },
-        { icon: ChartCandlestick, route: "/candlestick" },
-        { icon: Info, route: "/info" },
+        { name: "Staking", icon: Wallet, route: "/" },
+        { name: "Swap", icon: ArrowDownUp, route: "/swap" },
+        { name: "AI", icon: BrainCircuit, route: "/ai" },
+        { name: "Market", icon: ChartCandlestick, route: "/market" },
+        { name: "Help", icon: Info, route: "/info" },
       ].map((obj, index) => (
-        <Button
-          key={index}
-          onClick={() => handleButtonClick(index, obj.route)}
+        <div
           className={cn(
-            "h-12 w-12 rounded-full text-white shadow-none font-bold bg-accent",
-            activeIndex === index ? "active:bg-accent" : "bg-accent"
+            `w-full h-full flex flex-col text-center text-sm pb-4`,
+            activeIndex === index ? "border-t-2 border-primary" : ""
           )}
-          style={{
-            transform:
-              activeIndex === index ? "translateY(-22px)" : "translateY(0)", // Slight upward shift
-            backgroundColor: activeIndex === index ? "#272729" : "transparent",
-            transition: "transform 0.4s ease", // Smooth transition
-          }}
+          key={index}
         >
-          <obj.icon />
-        </Button>
+          <obj.icon
+            key={index}
+            onClick={() => handleButtonClick(index, obj.route)}
+            className={cn(
+              "size-5 text-primary-foreground m-auto shadow-none font-bold bg-accent rounded-none ",
+              activeIndex === index ? "active:bg-accent" : "bg-accent"
+            )}
+          />
+          <p className="text-muted-foreground">{obj.name}</p>
+        </div>
       ))}
     </div>
   );
