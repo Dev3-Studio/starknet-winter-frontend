9b0f5da04af51d841809eb29ceb373d18ae11b87
David <david.baldachin@gmail.com>
2025-01-09 19:51:28 +0200

    test name.

--- components/StakePage.tsx
+++ components/StakePage.tsx
@@ -15,15 +15,12 @@ import {
 const notifications = [
     {
         title: "Your call has been confirmed.",
-        description: "1 hour ago",
     },
     {
         title: "You have a new message!",
-        description: "1 hour ago",
     },
     {
         title: "Your subscription is expiring soon!",
-        description: "2 hours ago",
     },
 ]
 
@@ -59,15 +56,12 @@ export default function StakeCard ({ className, ...props }: CardProps) {
                     {notifications.map((notification, index) => (
                         <div
                             key={index}
-                            className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0"
+                            className="mb-4 grid grid-cols-[25px_1fr] items-start pt-4 pb-4 last:mb-0 last:pb-0"
                         >
                             <span className="flex h-2 w-2 translate-y-1 rounded-full bg-sky-500" />
                             <div className="space-y-1">
                                 <p className="text-sm font-medium leading-none">
                                     {notification.title}
-                                </p>
-                                <p className="text-sm text-muted-foreground">
-                                    {notification.description}
                                 </p>
                             </div>
                         </div>
