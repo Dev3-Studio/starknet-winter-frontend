1270f44347829507c11d0d6d0c228d18bd187c61
ColourfulMelon <jordynnitch@gmail.com>
2025-01-10 21:44:56 +0200

    chat layout improvement

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -5,12 +5,18 @@ import ChatBubble from '@/components/ChatBubble';
 
 export default function Chat(){
     
+    const messages = [
+        'message1',
+        'message2',
+        'message3'
+    ]
+    
     return(
         <div>
             <h1 className="pb-2 text-center">Chat</h1>
             
-            <div className="min-h-full overflow-y-scroll">
-                <ChatBubble text="test" side="left"/>
+            <div className="min-h-full overflow-y-scroll px-2 grid gap-2">
+                {messages.map((message, index) => <ChatBubble text={message} key={index} side='right'/>)}
             </div>
             
             
