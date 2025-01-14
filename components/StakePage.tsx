9251da92e0618d7bf72d8d17979839616a55f776
David <david.baldachin@gmail.com>
2025-01-14 10:56:17 +0200

    moved code into new component and added image for Starknet.

--- components/StakePage.tsx
+++ components/StakePage.tsx
@@ -12,19 +12,13 @@ import {
     CardTitle,
 } from "@/components/shadcn/card"
 import { ConnectWalletButton } from '@/components/ConnectWalletButton';
-import {StakeUnstakeToggle} from '@/components/StakeUnstakeButton';
+import { StakeUnstakeToggle } from '@/components/StakeUnstakeButton';
+import { DollarSection } from "@/components/DollarSection";
 
 
 type CardProps = React.ComponentProps<typeof Card>
 
 export default function StakeCard ({ className, ...props }: CardProps) {
-    const conversionRate = 10;
-    const [starkcoinAmount, setStarkcoinAmount] = useState<string>("");
-
-    const dollarAmount = setStarkcoinAmount
-        ? (parseFloat(starkcoinAmount) * conversionRate).toFixed(2)
-        : "0.00";
-
     return (
         <Card className={cn("w-[380px]", className)} {...props}>
             <CardHeader>
@@ -33,44 +27,7 @@ export default function StakeCard ({ className, ...props }: CardProps) {
                 </CardTitle>
             </CardHeader>
             <CardContent className="grid gap-4">
-
-                {/* to do add to components.*/}
-
-                <div
-                    className="flex flex-row items-center justify-between bg-contrast-0a p-2 rounded-lg mb-2 border border-solid border-contrast-0 focus-within:border-contrast-4 hover:bg-contrast-0">
-                    {/* Icon Section */}
-                    <div className="flex flex-col">
-                        <div className="flex flex-row items-center gap-x-3">
-                            <p className="text-sm font-bold text-contrast-5">StarkCoin</p>
-                        </div>
-                    </div>
-                    {/* Input Section */}
-                    <div className="flex flex-col items-end">
-                        <div className="tooltip_inputWrap___XCbD">
-                            <input
-                                className="numericInput_input__Wo6Z3 !bg-transparent !p-0 !h-fit !m-0 !text-right !text-xl !text-contrast-5 !font-bold focus:outline-none focus:ring-0"
-                                tabIndex="1"
-                                type="text"
-                                inputMode="decimal"
-                                pattern="[0-9]*"
-                                step="any"
-                                title=""
-                                min="0"
-                                max="1000000"
-                                placeholder="0.00"
-                                value={starkcoinAmount}
-                                onChange={(e) => setStarkcoinAmount(e.target.value)}
-                            />
-                        </div>
-                        <div className="flex justify-end">
-                            <p className="text-sm font-normal text-right text-contrast-3">
-                                ${starkcoinAmount ? dollarAmount : "0.00"}
-                            </p>
-                        </div>
-                    </div>
-                </div>
-
-
+                <DollarSection/>
                 <div className="w-full">
                     {/*<ConnectWalletButton/>*/}
                     <button className="w-full bg-purple-500 px-8 py-2 rounded-[8px]">
@@ -80,7 +37,7 @@ export default function StakeCard ({ className, ...props }: CardProps) {
 
                 <div className="flex items-center justify-between py-2 border-b border-contrast-0">
                     <p className="text-sm text-contrast-5 font-bold">Transaction Fee</p>
-                    <p className="text-sm text-contrast-5 font-bold">0.00</p>
+                    <p className="text-sm text-contrast-5 font-bold">$0.00</p>
                 </div>
 
             </CardContent>
--- components/StakeUnstakeButton.tsx
+++ components/StakeUnstakeButton.tsx
@@ -10,13 +10,13 @@ export const StakeUnstakeToggle = () => {
             className="inline-flex items-center border border-muted rounded-md px-1 py-1"
             style={{ outline: "none" }}
         >
+
             {/* Stake Button */}
             <button
                 type="button"
                 role="tab"
                 aria-selected={activeTab === "stake"}
-                className={`flex items-center justify-center whitespace-nowrap rounded-md px-4 py-2 text-sm font-medium transition-all
-          ${
+                className={`flex items-center justify-center whitespace-nowrap rounded-md px-4 py-2 text-sm font-medium transition-all ${
                     activeTab === "stake"
                         ? "bg-primary text-primary-foreground shadow-md"
                         : "text-muted-foreground hover:text-primary"
@@ -34,8 +34,7 @@ export const StakeUnstakeToggle = () => {
                 type="button"
                 role="tab"
                 aria-selected={activeTab === "unstake"}
-                className={`flex items-center justify-center whitespace-nowrap rounded-md px-4 py-2 text-sm font-medium transition-all
-          ${
+                className={`flex items-center justify-center whitespace-nowrap rounded-md px-4 py-2 text-sm font-medium transition-all ${
                     activeTab === "unstake"
                         ? "bg-primary text-primary-foreground shadow-md"
                         : "text-muted-foreground hover:text-primary"
