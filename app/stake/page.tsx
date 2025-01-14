7c98978447db6c83202bf311f5fb76e68dc14265
David <david.baldachin@gmail.com>
2025-01-14 13:36:12 +0200

    added toast from schadcn properly.

--- app/stake/page.tsx
+++ app/stake/page.tsx
@@ -1,10 +1,12 @@
 import StakeCard from "@/components/StakePage";
+import {Toaster} from "@/components/Toaster";
 
 
 export default function StakePage () {
     return (
         <div style={{display: "flex", justifyContent: "center", alignItems: "center", height: "100vh"}}>
             <StakeCard/>
+            <Toaster />
         </div>
     );
 }
