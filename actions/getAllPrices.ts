fe1635104447f42c929c23d73ff69daa3161f6b9
ColourfulMelon <jordynnitch@gmail.com>
2025-01-14 18:48:47 +0200

    swap page updates

---
+++ actions/getAllPrices.ts
@@ -0,0 +1,19 @@
+'use server';
+import assetList from '@/public/AssetList.json';
+import { getAssetPriceMedian } from './findPrice';
+
+async function getAllPricesFormatted() {
+    
+    return await Promise.all(
+        assetList.map(async (asset) => {
+            const price = await getAssetPriceMedian(asset.PairID, asset.Decimals);
+            return {
+                Name: asset.Name,
+                Ticker: asset.Ticker,
+                priceInCrypto: price.priceInCrypto,
+                priceInUSD: price.priceInUSD,
+            };
+        }),
+    );
+}
+export { getAllPricesFormatted };
