e7fc18e25a3f9724147f941c4212f0e323ed32e8
ColourfulMelon <jordynnitch@gmail.com>
2025-01-17 13:57:54 +0200

    add quoting functionality to ai chat

--- langchain/index.ts
+++ langchain/index.ts
@@ -30,10 +30,10 @@ const stakeSchema = z.object({
 })
 
 const swapSchema = z.object({
-    token1: z.string().describe('The token that will be sent'),
-    token2: z.string().describe('The token that will be received'),
-    amountIn: z.number().optional().describe('The amount of token1 to swap'),
-    amountOut: z.number().optional().describe('The amount of token2 to receive'),
+    tokenIn: z.string().describe('The token that will be sent'),
+    tokenOut: z.string().describe('The token that will be received'),
+    amountIn: z.number().optional().describe('The amount of tokenIn to swap'),
+    amountOut: z.number().optional().describe('The amount of tokenOut to receive'),
 })
 
 const stakeTool = tool(
@@ -60,6 +60,8 @@ const systemPrompt = new SystemMessage(`You are Tele. Your job is to take a user
 Choose the most appropriate action based on the user's request.
 Identify the most appropriate token based on the user's description. The token will be a cryptocurrency specified by the user.
 If the user says to "buy" or "sell" tokens, swap with the stark token as the other token (this is the native token for starknet).
+If the user says to "swap" x tokens for y tokens, swap amountIN tokenIn for amountOut tokenOut.
+If the user only specifies amountIn or amountOut, leave the other field blank. If neither field is specified, leave both fields blank.
 The only token that can be staked is the stark token. If the user says to "stake" tokens, stake with the stark token.
 If they mention staking another token, reply that only the stark token can be staked.
 `)
