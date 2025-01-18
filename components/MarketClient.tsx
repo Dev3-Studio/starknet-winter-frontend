5f5f4543753e175ff4d9e1072cde2c35ec9fbc62
io <stuart@kieser.co.za>
2025-01-18 22:55:24 +0200

    feat: Replace TokenPrice component with MarketClient for improved market display; add TokenPriceCard and TokenPriceList components

---
+++ components/MarketClient.tsx
@@ -0,0 +1,75 @@
+'use client';
+
+import { useState } from 'react';
+import { Button } from '@/components/shadcn/button';
+import { AlignJustifyIcon, Grid2X2Icon } from 'lucide-react';
+import TokenPrice from '@/components/TokenPriceCard';
+import TokenPriceCard from '@/components/TokenPriceCard';
+import TokenPriceList from './TokenPriceList';
+
+type MarketClientProps = {
+  prices: {
+    Name: string;
+    priceInUSD: number;
+  }[];
+};
+
+export default function MarketClient({ prices }: MarketClientProps) {
+  const [toggle, setToggle] = useState(true);
+
+  function getTokenImage(token: string) {
+    return `./${token}.webp`;
+  }
+
+  function handleToggle() {
+    setToggle(!toggle);
+  }
+
+  const MarketCards = () => {
+    return (
+      <div className='grid grid-cols-3 gap-4 p-4'>
+        {prices.map((asset, index) => (
+          <TokenPriceCard
+            key={index}
+            token={asset.Name}
+            image={getTokenImage(asset.Name)}
+            price={asset.priceInUSD}
+            className='card'
+          />
+        ))}
+      </div>
+    );
+  };
+
+  const Marketlist = () => {
+    return (
+      <div className='flex flex-rows flex-wrap gap-4 px-4 w-md mx-4 mt-4 rounded-t-2xl bg-secondary'>
+        {prices.map((asset, index) => (
+          <TokenPriceList
+            key={index}
+            token={asset.Name}
+            image={getTokenImage(asset.Name)}
+            price={asset.priceInUSD}
+            className='list'
+          />
+        ))}
+      </div>
+    );
+  };
+
+  return (
+    <div className=''>
+      <div className='flex flex-rows overflow-clip p-2 place-content-end'>
+        <Button
+          onClick={handleToggle}
+          className='bg-transparent hover:bg-secondary'
+        >
+          {toggle ? <AlignJustifyIcon /> : <Grid2X2Icon />}
+        </Button>
+      </div>
+      <div className='w-screen h-[calc(100vh-133px)] overflow-x-auto'>
+        {toggle ? <MarketCards /> : <Marketlist />}
+      </div>
+    </div>
+  );
+}
