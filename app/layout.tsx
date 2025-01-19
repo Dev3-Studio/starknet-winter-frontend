4050d09c6b60813d8d393e7d81ae82c6bf90706e
Xirynx <64374129+Xirynx@users.noreply.github.com>
2025-01-19 02:24:47 +0200

    styling: Add background image to app

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,11 @@ export default function RootLayout({
             <html lang="en">
                 <body className={`${inter.className} antialiased h-dvh`}>
                     <ContextProvider>
+                        <div
+                            className="absolute inset-0 -z-10 h-full w-full bg-background bg-[linear-gradient(to_right,hsla(var(--primary),0.4)_1px,transparent_1px),linear-gradient(to_bottom,hsla(var(--primary),0.4)_1px,transparent_1px)] bg-[size:6rem_4rem]">
+                            <div
+                                className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_800px_at_100%_200px,hsla(var(--background)),transparent)]"></div>
+                        </div>
                         <div className="grid grid-rows-[auto,5rem,0] h-full">
                             {children}
                             <div>
