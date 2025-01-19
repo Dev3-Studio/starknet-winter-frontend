35d302b8b2aca2004922e4478f6fc4eec7ef6d8f
Xirynx <64374129+Xirynx@users.noreply.github.com>
2025-01-19 16:53:16 +0200

    feat: add useTokenPrice hook

---
+++ hooks/useTokenPrice.tsx
@@ -0,0 +1,24 @@
+import { useQuery } from '@tanstack/react-query';
+import { useArgentTelegram } from '@/hooks/useArgentTelegram';
+import pragmaTokens from '@/public/pragmaTokens.json';
+import { getAssetPriceMedian } from '@/actions/findPrice';
+
+export const useTokenPrice = ({ pragmaFeedId }: { pragmaFeedId: string }) => {
+    const { account } = useArgentTelegram();
+    const { data: price } = useQuery({
+        queryKey: ['getAssetMedianPrice'],
+        initialData: () => 0,
+        queryFn: async () => {
+            if (!account) return 0;
+            const asset = pragmaTokens.find((p) => p.PairID === pragmaFeedId);
+            if (!asset) return 0;
+            const { priceInUSD } = await getAssetPriceMedian(pragmaFeedId, asset.Decimals);
+            return priceInUSD;
+        },
+        refetchOnMount: true,
+        refetchOnReconnect: true,
+        refetchOnWindowFocus: true,
+        refetchInterval: 3000,
+    });
+    return price;
+};
