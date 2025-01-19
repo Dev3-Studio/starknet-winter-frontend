a8b7fbcca84d81bce356163c920b3c235a774921
Xirynx <64374129+Xirynx@users.noreply.github.com>
2025-01-19 17:00:17 +0200

    update: Disable telegram swipe behaviour

--- context/index.tsx
+++ context/index.tsx
@@ -3,6 +3,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React, { type ReactNode, useEffect } from 'react';
 import { init } from '@telegram-apps/sdk-react';
+import { swipeBehavior } from '@telegram-apps/sdk';
 
 // Set up queryClient
 const queryClient = new QueryClient();
@@ -10,6 +11,9 @@ const queryClient = new QueryClient();
 function ContextProvider({ children }: { children: ReactNode }) {
     useEffect(() => {
         init();
+        if (swipeBehavior.isSupported()) {
+            swipeBehavior.disableVertical();
+        }
     }, []);
     return (
         <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
