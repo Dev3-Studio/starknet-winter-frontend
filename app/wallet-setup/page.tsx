8f8b586d8929238e6cb574e49c54cd55bae7800e
ColourfulMelon <jordynnitch@gmail.com>
2025-01-20 03:11:48 +0200

    fix checkAddressDeployed function

--- app/wallet-setup/page.tsx
+++ app/wallet-setup/page.tsx
@@ -6,10 +6,10 @@ export default function WalletPage() {
     }
     return (
         <div className="p-4">
-            <h1 className="text-2xl mt-1 text-center">Note:</h1>
+            <h1 className="text-2xl mt-1 text-center pt-10">Note:</h1>
             
-            <p className="">Your argent wallet is not yet deployed. You can deploy it by accessing your wallet from the
-                <span onClick={handleClick} className="text-primary cursor-pointer"> Argent wallet app</span> and performing any transaction.
+            <p className="px-2 text-center">Your argent wallet is not yet deployed. You can deploy it by accessing your wallet from the
+                <span onClick={handleClick} className="text-primary cursor-pointer "> Argent wallet app</span> and performing any transaction.
                 E.g send a small amount of stark to yourself.</p>
            
         </div>
